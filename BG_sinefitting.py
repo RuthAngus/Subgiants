405f13af8233b577a459f42770656eac94225ed9
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-10-20 16:11:43 -0400

    restructuring

--- BG_sinefitting.py
+++ BG_sinefitting.py
@@ -7,27 +7,37 @@ from rc_params import plot_params
 from colors import plot_colors
 ocol = plot_colors()
 
-def sinefit(freqs, amps, xs):
-    sine = np.zeros_like(xs)
-    for i in range(len(freqs)):
-        sine += amps[i] * np.sin(2*np.pi*freqs[i]*xs)
-    return sine
+def sinefit(xs, freq, amp, phase):
+    signal = np.zeros(len(xs))
+    for i in range(len(freq)):
+        print amp[i], freq[i], phase[i]
+        signal += amp[i] * np.sin(2*np.pi*freq[i]*xs - phase[i])
+        print signal
+    return signal
 
-dfreq, dfreq_err, freq, amp, amp_err = \
-        np.genfromtxt("/Users/angusr/Python/Subgiants/BG_freqs.txt",
-                      skip_header=1).T
+# dfreq, dfreq_err, freq, amp, amp_err = \
+#         np.genfromtxt("/Users/angusr/Python/Subgiants/BG_freqs.txt",
+#                       skip_header=1).T
+
+# load period04 fitted frequencies
+freq, amp, phase = np.genfromtxt('period04_results.txt',
+                                 skip_header=1).T
 
 # load flux data
 BG = BetaGem()
 x = BG.fHJD
 y, yerr = BG.flux-np.median(BG.flux), BG.flux_err
 
+plt.clf()
+plt.errorbar(x, y, yerr=yerr, fmt='k.', capsize=0, ecolor='.8')
+plt.show()
+
 # load rv data
 rvx, rv, rverr = BG.rvHJD-2450000, BG.rv, BG.rv_err
-xs = np.linspace(min(rvx), max(rvx), 100)
+xs = np.linspace(min(rvx), max(rvx), 100000)
 
 plt.clf()
 plt.errorbar(rvx, rv, yerr=rverr, fmt='k.', capsize=0, ecolor='.8')
-plt.plot(xs, sinefit(dfreq, amp, xs))
+plt.plot(xs, sinefit(xs, freq, amp, phase))
 plt.xlim(77.48, 77.7)
 plt.show()
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Subgiants
 =========
 
 Investigating the population of planets around subgiants
+
+BG_sinefitting.py - fourier analysis
+BetaGem.py - GP fitting
