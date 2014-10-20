5bf6bf2da12ba4b142898dc71af82c29d7cbe2fe
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-10-20 16:11:31 -0400

    first commit

---
+++ feasibility_tests.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scaling_relations import nu_max, delta_nu
+import astero
+data = astero.astero()
+
+# load astero data
+m = data.bm
+m_errp = data.bm_errp
+m_errm = data.bm_errm
+r = data.br
+r_errp = data.br_errp
+r_errm = data.br_errm
+logg = data.blogg
+logg_errp = data.blogg_errp
+logg_errm = data.blogg_errm
+teff = data.bteff
+teff_err = data.bteff_err
+
+mlower = 1.8
+l = m > mlower
+ms, ms_errp, ms_errm, rs, rs_errp, rs_errm, teffs, teffs_err = \
+        m[l], m_errp[l], m_errm[l], r[l], r_errp[l], r_errm[l], \
+        teff[l], teff_err[l]
+
+print ms, "+", ms_errp, "-", ms_errm
+print rs, "+", rs_errp, "-", rs_errm
+print teffs, "+/-", teffs_err
+
+nm = nu_max(ms, rs, teffs)
+nm_errp = nu_max(ms+ms_errp, rs+rs_errp, teffs+teffs_err)
+nm_errm = nu_max(ms-ms_errm, rs-rs_errm, teffs-teffs_err)
+
+dn = delta_nu(ms, rs)
+dn_errp = delta_nu(ms+ms_errp, rs+rs_errp)
+dn_errm = delta_nu(ms-ms_errm, rs-rs_errm)
+
+print nm, '+', nm_errp-nm, '-', nm-nm_errm
+print dn, '+', dn_errp-dn, '-', dn-dn_errm
---
+++ scaling_relations.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def nu_max(M, R, Teff):
+    # M and R in Solar units, Teff in K
+    # returns mHz
+    return M / (R**2 * np.sqrt(Teff/5777.)) * 3.05
+
+def delta_nu(M, R):
+    # M and R in Solar units, Teff in K
+    # returnsuuHz
+    return np.sqrt(M) * R**(-3./2.) * 134.9
