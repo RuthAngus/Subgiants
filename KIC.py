fc42da3afd4b55a0889763ca81cc5144da4cef8c
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-10-25 10:47:41 -0400

    generalising

--- KIC.py
+++ KIC.py
@@ -1,43 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pyfits
-from scipy.signal.spectral import lombscargle
 import glob
+import kplr
+from scipy.signal.spectral import lombscargle
 from scaling_relations import nu_max, delta_nu
 from astero import astero
-astr = astero()
-
 from rc_params import plot_params
 from colors import plot_colors
+
+client = kplr.API()
 plot_params = plot_params()
 ocols = plot_colors()
-
-# KIC 5955122
-# mass = 1.28 + 0.11 - 0.15
-# radius = 2.14 + 0.07 - 0.11
-# teff = 5930 +/- 55 or 5966 +/- 162
-m, r, t = 1.28, 2.14, 5930
-print 'KIC 5955122'
-print 'nu_max = ', nu_max(m, r, t)*1e3, 'uHz'
-print 'dnu = ', delta_nu(m, r), 'uHz'
-
-print 'KIC 2010607'
-m, r, t = 1.37, 2.42, 6361
-print 'nu_max = ', nu_max(m, r, t)*1e3, 'uHz'
-print 'dnu = ', delta_nu(m, r), 'uHz'
-
-print 'KIC 1725815'
-m, r, t = 1.43, 2.02, 6550
-print 'nu_max = ', nu_max(m, r, t)*1e3, 'uHz'
-print 'dnu = ', delta_nu(m, r), 'uHz'
-
-print 'KIC 1435467'
-m, r, t = 1.16, 1.63, 6433
-print 'nu_max = ', nu_max(m, r, t)*1e3, 'uHz'
-print 'dnu = ', delta_nu(m, r), 'uHz'
-
-print 'KIC 2309595'
-m, r, teff = 1.14, 2.40, 5238
+astr = astero()
 
 i = 0
 KID, m, r, teff = int(astr.iKID[i]), astr.im[i], \
@@ -46,14 +21,21 @@ KID, m, r, teff = int(astr.iKID[i]), astr.im[i], \
 print 'KIC ', KID
 print m, r, teff
 print 'nu_max = ', nu_max(m, r, teff)*1e3, 'uHz'
-print 'dnu = ', delta_nu(m, r), 'uHeffz'
+print 'dnu = ', delta_nu(m, r), 'uHz'
 raw_input('enter')
 
-KID = '00%s' % KID
-
 # load data, median normalise and join together
-lc_files = glob.glob("/Users/angusr/.kplr/data/lightcurves/%s/*_slc.fits" % KID)
+lc_files = glob.glob("/Users/angusr/.kplr/data/lightcurves/%s/*_slc.fits"
+                     % str(KID).zfill(9))
+# check to see whether you've already downloaded data...
 print len(lc_files), ' files'
+if len(lc_files) == 0:
+    star = client.star("%s" % KID)
+    lcs = star.get_light_curves(short_cadence=True, fetch=True)
+    lc_files = glob.glob("/Users/angusr/.kplr/data/lightcurves/%s/*_slc.fits"
+                         % str(KID).zfill(9))
+print lc_files
+
 for i, lc_file in enumerate(lc_files):
     hdulist = pyfits.open(lc_file)
     tbdata = hdulist[1].data
--- chiron_dwarfs.py
+++ chiron_dwarfs.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io.idl import readsav
 import idlsave
-import glob
 from rc_params import plot_params
 from colors import plot_colors
 params = plot_params()
@@ -13,15 +11,15 @@ DIR = '/Users/angusr/Python/Subgiants'
 stars = np.genfromtxt('%s/data/star_names.txt' % DIR)
 rv_files = glob.glob('%s/data/vst*.dat' % DIR)
 
-for star in stars:
+plt.clf()
+for i, star in enumerate(stars):
     data = idlsave.read("%s/data/vst%s.dat" % (DIR, int(star)))
 
     t = data.cf3['JD']
     rv = data.cf3['MNVEL']
     rv_err = data.cf3['ERRVEL']
 
-    plt.clf()
+    plt.subplot(len(stars), 1, i+1)
     plt.errorbar(t, rv, yerr=rv_err, capsize=0, ecolor='.8', fmt='k.')
-    plt.xlabel("$\mathrm{JD}$")
-    plt.ylabel("$\mathrm{RV~(ms^{-1})}$")
-    plt.savefig('%s/figures/%s' % (DIR, int(star)))
+
+plt.savefig("%s/figures/all_stars" % DIR)
