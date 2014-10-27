f5ad889e07db18d554fa388204b0acdd626bf937
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-10-26 22:40:11 -0400

    making things betters

--- BG_sinefitting.py
+++ BG_sinefitting.py
@@ -11,6 +11,7 @@ from rc_params import plot_params
 from colors import plot_colors
 ocol = plot_colors()
 BG = BetaGem()
+from astero_modelling import MCMC
 
 # load period04 fitted frequencies
 # freq, amp, phase = \
@@ -31,38 +32,53 @@ BGteff_err = 150
 nm = nu_max(BGm, BGr, BGteff)/1e3
 dnu = delta_nu(BGm, BGr)/1e6
 
-# load rv data
+# load one section of rv data
 l = 38
-rvx, rv, rverr = BG.rvHJD[:l]-2450000, BG.rv[:l], BG.rv_err[:l]
-ys = fit_sine(rvx, rv, freq_day)
+rvx, rv, rverr = (BG.rvHJD[:l]-2450000)*24*60*60, BG.rv[:l], BG.rv_err[:l]
+# ys = fit_sine(rvx, rv, freq_day)
 
-# plt.clf()
-# plt.errorbar(rvx, rv, yerr=rverr, fmt='k.', capsize=0, ecolor='.8')
-# plt.plot(rvx, ys)
-# plt.xlabel('Time (s)')
-# plt.ylabel('RV')
-# plt.show()
+nf = 6
+freqs = np.arange(nm-nf*dnu, nm+nf*dnu, dnu)
+freqs = [nm]
+print freqs
 
-# load flux data
-# x = BG.fHJD
-x = BG.fHJD  # *24*60*60
-y, yerr = BG.flux-np.median(BG.flux), BG.flux_err
-ys = fit_sine(x, y, freq_day)
-
+ys = fit_sine(rvx, rv, freqs)
 plt.clf()
-plt.errorbar(x, y, yerr=yerr, fmt='k.', capsize=0, ecolor='.8')
-plt.plot(x, ys)
+plt.errorbar(rvx, rv, yerr=rverr, fmt='k.', capsize=0, ecolor='.8')
+plt.plot(rvx, ys)
 plt.xlabel('Time (s)')
-plt.ylabel('Flux')
+plt.ylabel('RV')
 plt.show()
 
-# compute lomb scargle
-fs = np.linspace(1e-3, 250e-3, 1000) # Hz
-# fs = np.linspace(.1, 30., 1000)
-ws = 2*np.pi*fs  # lombscargle uses angular frequencies
-pgram = sps.lombscargle(x, y, ws)
-plt.clf()
-plt.plot(ws/2./np.pi, pgram)
-# print ws[pgram==max(pgram)]/2./np.pi, 'Hz'
-# peaks = sps.find_peaks_cwt(pgram, np.arange(0.00001, 0.0002, 10))
+# load all rv data
+rvx, rv, rverr = (BG.rvHJD-2450000)*24*60*60, BG.rv, BG.rv_err
+
+# find mass, radius and teff
+theta = (BGm, BGr, BGteff)
+nfreqs = 6
+args = (rvx, rv, rverr, nfreqs)
+fig_labels = ["m", "r", "teff"]
+print MCMC(theta, args, 1000, 5000, BG, fig_labels)
+
+# # load flux data
+# x = BG.fHJD
+# y, yerr = BG.flux-np.median(BG.flux), BG.flux_err
+# ys = fit_sine(x, y, freq_day)
+
+# plt.clf()
+# plt.errorbar(x, y, yerr=yerr, fmt='k.', capsize=0, ecolor='.8')
+# plt.plot(x, ys)
+# plt.xlabel('Time (s)')
+# plt.ylabel('Flux')
 # plt.show()
+
+# # compute lomb scargle
+# fs = np.linspace(1e-3, 250e-3, 1000) # Hz
+# # fs = np.linspace(.1, 30., 1000)
+# ws = 2*np.pi*fs  # lombscargle uses angular frequencies
+# pgram = sps.lombscargle(x, y, ws)
+# plt.clf()
+# plt.plot(ws/2./np.pi, pgram)
+# # print ws[pgram==max(pgram)]/2./np.pi, 'Hz'
+# # peaks = sps.find_peaks_cwt(pgram, np.arange(0.00001, 0.0002, 10))
+# # plt.show()
