f90772c2b8f1b097017c7363e1a5f5fcb2abfe9c
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-11-11 19:36:45 -0500

    I think it's working

--- observing_strategy/observing_strategy.py
+++ observing_strategy/observing_strategy.py
@@ -8,91 +8,101 @@ from rc_params import plot_params
 reb, fbk = plot_params()
 from BGdata import BetaGem
 BG = BetaGem()
+from scaling_relations import nu_max, delta_nu
 
-# Load Beta Gem data
-t, rv, rv_err = BG.rvHJD-BG.rvHJD[0], BG.rv/np.median(BG.rv), \
-        BG.rv_err/np.median(BG.rv)
-nm_days = BG.nm*3600*24
-P = 1./nm_days
-l = 38
-# l = len(t)
-t, rv, rv_err = t[:l], rv[:l], rv_err[:l]
+def train_BG():
+    # Load Beta Gem data
+    t, rv, rv_err = BG.rvHJD-BG.rvHJD[0], BG.rv/np.median(BG.rv), \
+            BG.rv_err/np.median(BG.rv)
+    nm_days = BG.nm*3600*24
+    P = 1./nm_days
+    l = 38
+    # l = len(t)
+    t, rv, rv_err = t[:l], rv[:l], rv_err[:l]
 
-# train GP on Beta Gem (without white noise)
-theta = [15., 5., 1., P]
-k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[3])
-gp = george.GP(k)
-gp.compute(t, rv_err)
-# results = gp.optimize(t, rv, rv_err, dims=[0, 1, 2])[0]
-results = gp.optimize(t, rv, rv_err)[0]
-print np.exp(results)
+    # train GP on Beta Gem (without white noise)
+    theta = [15., 5., 1., P]
+    k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[3])
+    gp = george.GP(k)
+    gp.compute(t, rv_err)
+    # results = gp.optimize(t, rv, rv_err, dims=[0, 1, 2])[0]
+    results = gp.optimize(t, rv, rv_err)[0]
+    print np.exp(results)
 
-# plot BG data and result
-xs = np.linspace(min(t), max(t), 1000)
-mu, cov = gp.predict(rv, xs)
-plt.clf()
-plt.errorbar(t, rv, yerr=rv_err, **reb)
-plt.plot(xs, mu, color=ocols.blue)
-plt.xlabel('$\mathrm{Time~(days)}$')
-plt.ylabel('$\mathrm{RV~(ms}^{-1}\mathrm{)}$')
-std = np.sqrt(np.diag(cov))
-plt.fill_between(xs, mu+std, mu-std, **fbk)
-plt.savefig('BG_trained')
-
-# Generate time series with a GP
-interval = 48  # 96 for 1/4, 48 for 1/2, 24 for hr, 12 for 2 hr
-ndays = 10
-xs = np.linspace(0, ndays, interval*ndays) # one point every 1/2 hour
-yerr = np.ones_like(xs)*.01
-
-# Compute GP prior sample
-theta = [8.6969e+2, 1.725e-3, 1.654, P]
-# theta = results
-# theta = np.zeros(len(results)+1)
-# theta[:-1] = results
-# theta[-1] = P
-k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[3])
-gp = george.GP(k)
-gp.compute(xs, yerr)
-np.random.seed(1234)
-
-l = interval  # sample every day
-f = interval/24  # sample every hour
-samples = gp.sample(xs, 10)
-
-best_time = []
-for i, s in enumerate(samples):
-
-    # sample every 1/2, 1, 1.5, 2, hours
-    ms = np.arange(.5, 10, .5)  # sampling interval in hours
-    stds = []
-    for m in ms:
-
-        xs1, xs2, xs3 = xs[::l], xs[m::l], xs[2*m::l]
-        ss1, ss2, ss3 = s[::l], s[m::l], s[2*m::l]
-        yerrs1, yerrs2, yerrs3 = yerr[::l], yerr[m::l], yerr[2*m::l]
-
-        xss = np.vstack((xs1, xs2, xs3))
-        ss = np.vstack((ss1, ss2, ss3))
-        yerrs = np.vstack((yerrs1, yerrs2, yerrs3))
-        xmean = np.mean(xss, axis=0)
-        ymean = np.mean(ss, axis=0)
-
-        stds.append(np.std(ymean))
-
-    sdts = np.array(stds)
+    # plot BG data and result
+    xs = np.linspace(min(t), max(t), 1000)
+    mu, cov = gp.predict(rv, xs)
     plt.clf()
-    plt.subplot(2, 1, 1)
-    plt.plot(xs, s, color=ocols.blue)
-    plt.xlabel("$\mathrm{Time~(days)}$")
-    plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
-    plt.subplot(2, 1, 2)
-    plt.plot(ms, stds, color=ocols.orange,
-             label="$%s$" % ms[stds==min(stds)][0])
-    plt.xlabel("$\mathrm{Time~between~samples~(hours)}$")
-    plt.ylabel("$\mathrm{RMS}$")
-    plt.subplots_adjust(hspace=.3)
-    plt.legend()
-    plt.savefig('GP_%s' % i)
+    plt.errorbar(t, rv, yerr=rv_err, **reb)
+    plt.plot(xs, mu, color=ocols.blue)
+    plt.xlabel('$\mathrm{Time~(days)}$')
+    plt.ylabel('$\mathrm{RV~(ms}^{-1}\mathrm{)}$')
+    std = np.sqrt(np.diag(cov))
+    plt.fill_between(xs, mu+std, mu-std, **fbk)
+    plt.savefig('BG_trained')
 
-    best_time.append(ms[stds==min(stds)])
+def sampling_method(P, fname):
+
+    # Generate time series with a GP
+    interval = 48  # 96 for 1/4, 48 for 1/2, 24 for hr, 12 for 2 hr
+    ndays = 10
+    xs = np.linspace(0, ndays, interval*ndays) # one point every 1/2 hour
+    yerr = np.ones_like(xs)*.01
+
+    # Compute GP prior sample
+    theta = [8.6969e+2, 1.725e-3, 1.654, P]
+    # theta = results
+    # theta = np.zeros(len(results)+1)
+    # theta[:-1] = results
+    # theta[-1] = P
+    k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[3])
+    gp = george.GP(k)
+    gp.compute(xs, yerr)
+    np.random.seed(1234)
+
+    l = interval  # sample every day
+    f = interval/24  # sample every hour
+    samples = gp.sample(xs, 10)
+
+    best_time = []
+    for i, s in enumerate(samples):
+
+        # sample every 1/2, 1, 1.5, 2, hours
+        ms = np.arange(.5, 10, .5)  # sampling interval in hours
+        stds = []
+        for m in ms:
+
+            xs1, xs2, xs3 = xs[::l], xs[m::l], xs[2*m::l]
+            ss1, ss2, ss3 = s[::l], s[m::l], s[2*m::l]
+            yerrs1, yerrs2, yerrs3 = yerr[::l], yerr[m::l], yerr[2*m::l]
+
+            xss = np.vstack((xs1, xs2, xs3))
+            ss = np.vstack((ss1, ss2, ss3))
+            yerrs = np.vstack((yerrs1, yerrs2, yerrs3))
+            xmean = np.mean(xss, axis=0)
+            ymean = np.mean(ss, axis=0)
+
+            stds.append(np.std(ymean))
+
+        sdts = np.array(stds)
+        plt.clf()
+        plt.subplot(2, 1, 1)
+        plt.plot(xs, s, color=ocols.blue)
+        plt.xlabel("$\mathrm{Time~(days)}$")
+        plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
+        plt.subplot(2, 1, 2)
+        plt.plot(ms, stds, color=ocols.orange,
+                 label="$%s$" % ms[stds==min(stds)][0])
+        plt.xlabel("$\mathrm{Time~between~samples~(hours)}$")
+        plt.ylabel("$\mathrm{RMS}$")
+        plt.subplots_adjust(hspace=.3)
+        plt.legend()
+        plt.savefig('GP_%s_%s' % (i, fname))
+
+        best_time.append(ms[stds==min(stds)])
+
+if __name__ == "__main__":
+
+    Ps = np.linspace(1e-6, .1, 5)
+    for i, P in enumerate(Ps):
+        sampling_method(P, i)
--- sin_tests.py
+++ sin_tests.py
@@ -13,23 +13,41 @@ def fit_sine(x, y, w_true):
     ys += A[-1]
     return ys
 
+def fit_sine_err(x, y, yerr, w_true):
+    M = np.ones((len(x), 2*len(w_true)+1))
+    S = yerr**2 * np.eye(len(yerr))
+    for i in range(len(w_true)):
+        M[:, 2*i] = np.sin(w_true[i]*x)
+        M[:, 2*i+1] = np.cos(w_true[i]*x)
+    A = np.linalg.solve(M.T.dot(np.linalg.inv(S)).dot(M),
+                        (M.T.dot(np.linalg.inv(S)).dot(y)))
+    ys = np.zeros_like(x)
+    for i in range(len(w_true)):
+        ys += A[2*i]*np.sin(w_true[i]*x) + A[2*i+1]*np.cos(w_true[i]*x)
+    ys += A[-1]
+    return ys
+
 if __name__ == "__main__":
 
     # generate some data
-    A_true = [.1, .4, .6]
-    w_true = [2., 1., .5]
+    A_true = [10.1, .4, .6]
+    w_true = [20., 10., 5.]
     phi_true = [np.pi/3, np.pi/2., np.pi/6.]
     x = np.linspace(0, 20, 1000)
     y = np.zeros_like(x)
     for i in range(len(A_true)):
         y += A_true[i]*np.sin(w_true[i]*x + phi_true[i])
-    yerr = np.ones_like(x)*0.01
-#     yerr = np.ones_like(x)*10.0
-    y += 0.1 * np.random.randn(len(y))
-
-    ys = fit_sine(x, y, w_true)
 
     plt.clf()
+    plt.plot(x, y, 'm')
+
+    err = 1.
+    yerr = err * np.random.randn(len(y))
+    y += yerr * np.random.randn(len(y))
+    ys = fit_sine(x, y, w_true)
+    ys2 = fit_sine_err(x, y, yerr, w_true)
+
     plt.errorbar(x, y, yerr=yerr, fmt='k.', capsize=0, ecolor='.8')
     plt.plot(x, ys)
+    plt.plot(x, ys2, 'g')
     plt.show()
