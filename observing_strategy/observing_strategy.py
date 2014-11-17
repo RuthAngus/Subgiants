36010a5a5a6ba84cc3f1a8a9d5398bc608a67ced
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-11-17 15:48:57 -0500

    refactoring

--- observing_strategy/observing_strategy.py
+++ observing_strategy/observing_strategy.py
@@ -9,9 +9,12 @@ reb, fbk = plot_params()
 from BGdata import BetaGem
 BG = BetaGem()
 from scaling_relations import nu_max, delta_nu
+import scipy.signal as sps
 
 plotpar = {'legend.fontsize': 10}
 plt.rcParams.update(plotpar)
+
+mins = 5
 
 def train_BG():
     # Load Beta Gem data
@@ -44,7 +47,18 @@ def train_BG():
     plt.fill_between(xs, mu+std, mu-std, **fbk)
     plt.savefig('BG_trained')
 
-def sampling_method(P, fname):
+def sample_prior(theta, xs, yerr, nsamp):
+    # Compute GP prior sample
+    k = theta[0] * ExpSquaredKernel(theta[1]) * \
+            ExpSine2Kernel(theta[2], theta[3])
+    gp = george.GP(k)
+    gp.compute(xs, yerr)
+    np.random.seed(1234)
+    samples = gp.sample(xs, nsansamp)
+
+    return samples
+
+def dumb_sampling(fname):
 
     # Generate time series with a GP
     interval = 6*24  # 10 mins
@@ -54,19 +68,8 @@ def sampling_method(P, fname):
     yerr = np.ones_like(xs)*.01
 
     # Compute GP prior sample
-#     theta = [8.6969e+2, 1.725e-3, 1.654, P]
     theta = [8.6969, 1.725e-3, 1.654, P]
-    # theta = results
-    # theta = np.zeros(len(results)+1)
-    # theta[:-1] = results
-    # theta[-1] = P
-    k = theta[0] * ExpSquaredKernel(theta[1]) * \
-            ExpSine2Kernel(theta[2], theta[3])
-    gp = george.GP(k)
-    gp.compute(xs, yerr)
-    np.random.seed(1234)
-#     samples = gp.sample(xs, 2000)
-    samples = gp.sample(xs, 10)
+    samples = sample_prior(theta, xs, yerr)
 
     l = interval  # sample every day
     best_time = []
@@ -79,63 +82,157 @@ def sampling_method(P, fname):
         plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
         plt.subplot(2, 1, 2)
 
-        ms = np.array(range(24)) # from 0 to 120 minute intervals
-        stds, rms, rms2 = [], [], []
-        for m in ms:
-            xs1, xs2, xs3 = xs[::l], xs[m::l], xs[2*m::l]
-            ss1, ss2, ss3 = s[::l], s[m::l], s[2*m::l]
-            yerrs1, yerrs2, yerrs3 = yerr[::l], yerr[m::l], yerr[2*m::l]
+        times = 48
+        rms2 = np.zeros((times, times))
+        ms = np.array(range(times)) # from 0 to 120 minute intervals
+        for j in ms:
+            stds, rms = [], []
+            for m in ms:
+                xs1, xs2, xs3 = xs[j::l], xs[m+j::l], xs[2*m+j::l]
+                ss1, ss2, ss3 = s[j::l], s[m+j::l], s[2*m+j::l]
+                yerrs1, yerrs2, yerrs3 = yerr[j::l], yerr[m+j::l], \
+                        yerr[2*m+j::l]
 
-            xss = np.vstack((xs1, xs2, xs3))
-            ss = np.vstack((ss1, ss2, ss3))
-            yerrs = np.vstack((yerrs1, yerrs2, yerrs3))
-            ymean = np.mean(ss, axis=0)
+                xss = np.vstack((xs1, xs2, xs3))
+                ss = np.vstack((ss1, ss2, ss3))
+                yerrs = np.vstack((yerrs1, yerrs2, yerrs3))
+                ymean = np.mean(ss, axis=0)
 
-            stds.append(np.std(ymean))
-            rms.append(np.sqrt(np.mean(ymean**2)))
-            rms2.append(np.sqrt(np.mean(ss2**2)))
+                stds.append(np.std(ymean))
+                rms.append(np.sqrt(np.mean(ymean**2)))
 
-#             plt.clf()
-#             plt.plot(xs, s, color=ocols.blue)
-#             plt.plot(xs1, ss1, 'k.')
-#             plt.plot(xs2, ss2, 'k.')
-#             plt.plot(xs3, ss3, 'k.')
-#             plt.plot(xs2, ss2, 'mo')
-#             plt.plot(xs2, np.mean(ss, axis=0), '.', color=ocols.pink)
-#             plt.xlabel('$\mathrm{Time~(days)}$')
-#             plt.ylabel('$\mathrm{RV~(ms}^{-1}\mathrm{)}$')
-#             plt.savefig('observe')
+    #             plt.clf()
+    #             plt.plot(xs, s, color=ocols.blue)
+    #             plt.plot(xs1, ss1, 'k.')
+    #             plt.plot(xs2, ss2, 'k.')
+    #             plt.plot(xs3, ss3, 'k.')
+    #             plt.plot(xs2, ss2, 'mo')
+    #             plt.plot(xs2, np.mean(ss, axis=0), '.', color=ocols.pink)
+    #             plt.xlabel('$\mathrm{Time~(days)}$')
+    #             plt.ylabel('$\mathrm{RV~(ms}^{-1}\mathrm{)}$')
+    #             plt.savefig('observe')
 
-        stds, rms, rms2 = np.array(stds), np.array(rms), np.array(rms2)
-#         ll = stds==min(stds)
-        ll = rms==min(rms)
-        mins = 5
+            stds, rms = np.array(stds), np.array(rms)
+
+            plt.plot(ms*mins, rms, color=ocols.orange, alpha=.3)
+            rms2[:][j] = rms
+
+        mean_rms = np.mean(rms2, axis=0)
+        ll = mean_rms==min(mean_rms)
         lab = ms[ll][0]*mins
-
-#         diff = rms2-rms
-#         ll = diff==max(diff)
-
-#         diff = rms-rms2
-#         ll = diff==min(diff)
-
-        lab = ms[ll][0]*mins
-        plt.plot(ms*mins, rms, color=ocols.orange,
-                 label="$\mathrm{binned}$")
-        plt.plot(ms*mins, rms2, color=ocols.green,
-                 label='$\mathrm{per~night}$')
-#         plt.plot(ms*mins, rms-rms2, color=ocols.pink)
-#         plt.plot(ms*mins, diff, color=ocols.pink,
-#                   label="$\mathrm{min} = %s$" % lab)
-#         plt.plot(ms*mins, stds, color=ocols.green)
+        plt.plot(ms*mins, mean_rms, color=ocols.orange, linewidth=2,
+                label='$\mathrm{min~RMS}=%s$' % lab)
+        plt.subplots_adjust(hspace=.3)
         plt.xlabel("$\mathrm{Time~between~samples~(minutes)}$")
         plt.ylabel("$\mathrm{RMS}$")
-        plt.subplots_adjust(hspace=.3)
+        plt.xlim(0, times*mins)
         plt.legend()
+
+#         fms = sps.find_peaks_cwt(mean_rms, np.arange(1, 100))
+#         for fm in fms:
+#             print mean_rms[fm]*mins
+#             plt.axvline(mean_rms[fm]*mins, color=ocols.blue)
+
         plt.savefig('GP_%s_%s' % (i, fname))
-#         plt.show()
-#         raw_input('eter')
 
         best_time.append(lab)
+
+    return rms2, mean_rms, lab, best_time
+
+def obs_times(nmins, ndays, ntests, nsamples):
+    t = nmins/60./24  # 10 minutes in days
+    times = np.zeros((nsamples*ndays, ntests))
+    for i in range(ntests):
+        times[:, i] = np.sort(np.concatenate((np.arange(1, ndays-1),
+                        np.arange(1-(t*i), ndays-1-(t*i), 1),
+                        np.arange(1+(t*i), ndays-1+(t*i), 1))))
+    return times
+
+def smart_sampling(fname):
+
+    nmins = 10.  # interval between observations in minutes
+    ndays, ntests, nsamples = 12, 10, 3
+    xs = obs_times(nmins, ndays, ntests, nsamples)
+    yerr = np.ones_like(xs)*.01
+
+    # Compute GP prior sample
+    theta = [8.6969, 1.725e-3, 1.654, P]
+    nsamp = 1
+    samples = sample_prior(theta, xs[0], yerr, nsamp)
+    plt.clf()
+    plt.plot(xs[0], samples, colour=ocols.blue)
+    plt.show()
+    raw_input('enter')
+
+    for i, s in enumerate(samples):
+
+        plt.clf()
+        plt.subplot(2, 1, 1)
+        plt.plot(xs, s, color=ocols.blue)
+        plt.xlabel("$\mathrm{Time~(days)}$")
+        plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
+        plt.subplot(2, 1, 2)
+
+        times = 48
+        rms2 = np.zeros((times, times))
+        ms = np.array(range(times)) # from 0 to 120 minute intervals
+        for j in ms:
+            stds, rms = [], []
+            for m in ms:
+                xs1, xs2, xs3 = xs[j::l], xs[m+j::l], xs[2*m+j::l]
+                ss1, ss2, ss3 = s[j::l], s[m+j::l], s[2*m+j::l]
+                yerrs1, yerrs2, yerrs3 = yerr[j::l], yerr[m+j::l], \
+                        yerr[2*m+j::l]
+
+                xss = np.vstack((xs1, xs2, xs3))
+                ss = np.vstack((ss1, ss2, ss3))
+                yerrs = np.vstack((yerrs1, yerrs2, yerrs3))
+                ymean = np.mean(ss, axis=0)
+
+                stds.append(np.std(ymean))
+                rms.append(np.sqrt(np.mean(ymean**2)))
+
+    #             plt.clf()
+    #             plt.plot(xs, s, color=ocols.blue)
+    #             plt.plot(xs1, ss1, 'k.')
+    #             plt.plot(xs2, ss2, 'k.')
+    #             plt.plot(xs3, ss3, 'k.')
+    #             plt.plot(xs2, ss2, 'mo')
+    #             plt.plot(xs2, np.mean(ss, axis=0), '.', color=ocols.pink)
+    #             plt.xlabel('$\mathrm{Time~(days)}$')
+    #             plt.ylabel('$\mathrm{RV~(ms}^{-1}\mathrm{)}$')
+    #             plt.savefig('observe')
+
+            stds, rms = np.array(stds), np.array(rms)
+
+            plt.plot(ms*mins, rms, color=ocols.orange, alpha=.3)
+            rms2[:][j] = rms
+
+        mean_rms = np.mean(rms2, axis=0)
+        ll = mean_rms==min(mean_rms)
+        lab = ms[ll][0]*mins
+        plt.plot(ms*mins, mean_rms, color=ocols.orange, linewidth=2,
+                label='$\mathrm{min~RMS}=%s$' % lab)
+        plt.subplots_adjust(hspace=.3)
+        plt.xlabel("$\mathrm{Time~between~samples~(minutes)}$")
+        plt.ylabel("$\mathrm{RMS}$")
+        plt.xlim(0, times*mins)
+        plt.legend()
+
+#         fms = sps.find_peaks_cwt(mean_rms, np.arange(1, 100))
+#         for fm in fms:
+#             print mean_rms[fm]*mins
+#             plt.axvline(mean_rms[fm]*mins, color=ocols.blue)
+
+        plt.savefig('GP_%s_%s' % (i, fname))
+
+        best_time.append(lab)
+
+    return rms2, mean_rms, lab, best_time
+
+
+def sampling_method(P, fname):
+    rms2, mean_rms, lab, best_time = smart_sampling(fname)
     return np.array(best_time)
 
 if __name__ == "__main__":
@@ -143,12 +240,13 @@ if __name__ == "__main__":
     print 'Beta Gem = ', 1./BG.nm/3600.
 
 #     ps = [1, 2, 3]
-    ps = [1]
+    ps = [2]
     for p in ps:
+        times = []
         print p
         P = p/24.
-        times = sampling_method(P, 'test')
-#         plt.clf()
-#         plt.hist(times, 20, color='.2', edgecolor='w')
-#         plt.xlabel('$\mathrm{Time~interval~(mins)}$')
-#         plt.savefig('hist%s' % p)
+        times.append(sampling_method(P, 'test'))
+
+        plt.clf()
+        plt.hist(times, color='.3', edgecolor='w')
+        plt.savefig('hist%s' % p)
