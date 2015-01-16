26b1ce6daed6add0b178b5a52e94800feacce9bc
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-01-16 15:38:21 -0500

    about to tidy up

--- synthesise/synthesise.py
+++ synthesise/synthesise.py
@@ -6,6 +6,7 @@ from rc_params import plot_params
 reb, fbt = plot_params()
 from regularised_sine_fitting import fit_sine_reg
 from scipy.signal import butter, lfilter, lombscargle
+from sin_tests import show_sine
 
 # load light curve. return x, y, yerr
 def load_data(fname):
@@ -37,10 +38,8 @@ def convert_to_rv(flux, flux_err, teff, t_err):
     return rv, rv_err
 
 # high-pass filter
-def highpass_filter(data, cutoff, fs, order=5):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+def highpass_filter(data, f_crit, order=5):
+    b, a = butter(order, f_crit, btype='high', analog=False)
     y = lfilter(b, a, data)
     return y
 
@@ -52,9 +51,9 @@ def load_freqs(fname):
 
 # fit frequencies. for now use all of them?
 def fit_freqs(t, rv, rv_err, freqs, reg):
-    t *= 24*3600  # convert to seconds
-    ys, A = fit_sine_reg(t, rv, rv_err, freqs*2*np.pi, reg)
-    return t, ys
+    ts = t*24*3600  # convert to seconds
+    ys, A = fit_sine_reg(ts, rv, rv_err, freqs*2*np.pi, reg)
+    return ts, ys, A
 
 if __name__ == "__main__":
 
@@ -71,9 +70,9 @@ if __name__ == "__main__":
         fname = str(int(kid[i]))
         x, y, yerr = load_data(fname)
 
-        print "truncate"
-        l = 10000
-        x, y, yerr = x[:l], y[:l], yerr[:l]
+#         print "truncate"
+#         l = 10000
+#         x, y, yerr = x[:l], y[:l], yerr[:l]
 
         print "convert to rv"
         rv, rv_err = convert_to_rv(y, yerr, teff[i], t_err[i])
@@ -83,10 +82,9 @@ if __name__ == "__main__":
         for i in range(len(rv)):
             rv64[i] = rv[i].astype('float64')
 
-        # compute periodogram
-        f = np.logspace(.01, 2, num=1000)
-        pgram = lombscargle(x, rv64, f*2*np.pi)
-
+#         # compute periodogram
+#         f = np.logspace(.01, 2, num=1000)
+#         pgram = lombscargle(x, rv64, f*2*np.pi)
 #         # load freqs
 #         freqs = load_freqs(fname)
 #         plt.clf()
@@ -97,30 +95,44 @@ if __name__ == "__main__":
 #         plt.xlabel("freq")
 
         # high pass filter
-        cutoff, fs = 1, 10.
-        filtered_rv = highpass_filter(rv64, cutoff, fs)
-        pgram = lombscargle(x, filtered_rv, f*2*np.pi)
+        freqs = load_freqs(fname)
+        # min(freqs) = 0.0005, max(freqs) = 0.001
+        f_crit = 0.0005
+        filtered_rv = highpass_filter(rv64, f_crit)
 
+#         pgram = lombscargle(x, filtered_rv, f*2*np.pi)
 #         plt.subplot(2, 1, 2)
 #         for freq in freqs:
 #             plt.axvline(freq*24*3600, color="r")
 #         plt.plot(f, pgram)
 #         plt.savefig('filter_test')
 
+        plt.clf()
+        plt.subplot(2, 1, 1)
+        plt.plot(x, rv, "k.")
+        plt.subplot(2, 1, 2)
+        plt.plot(x, filtered_rv, "k.")
+        plt.savefig("test")
+
+        print "fit regularised sine waves"
+        reg = 1000
+        ts, ys, A = fit_freqs(x, filtered_rv, rv_err, freqs[:1]*2*np.pi, reg)
+#         ts, ys, A = fit_freqs(x, rv, rv_err, freqs*2*np.pi, reg)
+
 #         plt.clf()
-#         plt.subplot(2, 1, 1)
-#         plt.plot(x, rv, "k.")
-#         plt.subplot(2, 1, 2)
 #         plt.plot(x, filtered_rv, "k.")
-#         plt.savefig("test")
-#         raw_input('enter')
-
-#         print "fit regularised sine waves"
-#         freqs = load_freqs(fname)
-#         reg = 0
-#         x, ys = fit_freqs(x, rv, rv_err, freqs*2*np.pi, reg)
-
-#         plt.clf()
-#         plt.plot(x, y, "k.")
 #         plt.plot(x, ys)
+# #         plt.xlim(x[0], x[500])
 #         plt.savefig("test")
+
+        fs = np.linspace(0.00000001, max(freqs), 1000)
+        pgram = lombscargle(ts, ys, fs*2*np.pi)
+
+        plt.clf()
+        for i in freqs:
+            plt.axvline(i, color="r")
+        plt.plot(fs, pgram)
+#         plt.ylim(0, 10000)
+        plt.ylim(0, 1)
+        plt.savefig("frequency_detection")
+        assert 0
