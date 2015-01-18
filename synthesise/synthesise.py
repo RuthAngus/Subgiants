81e84ff00ce740362c34fdf43fce65335885ebef
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-01-17 23:06:26 -0500

    debugging attempts

--- synthesise/synthesise.py
+++ synthesise/synthesise.py
@@ -35,6 +35,12 @@ def load_data(fname):
 # convert flux to rv
 def convert_to_rv(flux, flux_err, teff, t_err):
     rv, rv_err, dlL = sc_target.flux_rv(flux, flux_err, teff, t_err)
+
+    # remove weird errorbars
+    l = dlL==0
+    rv_err[l] = np.mean(rv_err[np.isfinite(rv_err)])
+    l = rv_err > 10
+    rv_err[l] = np.mean(rv_err[rv_err<10])
     return rv, rv_err
 
 # high-pass filter
@@ -49,12 +55,6 @@ def load_freqs(fname):
                           skip_header=1).T
     return freqs/1e6  # convert to Hz
 
-# fit frequencies. for now use all of them?
-def fit_freqs(t, rv, rv_err, freqs, reg):
-    ts = t*24*3600  # convert to seconds
-    ys, A = fit_sine_reg(ts, rv, rv_err, freqs*2*np.pi, reg)
-    return ts, ys, A
-
 if __name__ == "__main__":
 
     DIR = "/Users/angusr/Python/Subgiants"
@@ -66,73 +66,50 @@ if __name__ == "__main__":
     for i in range(len(kid)):
         print kid[i]
 
-        print "load fits file"
+        # load fits file
         fname = str(int(kid[i]))
         x, y, yerr = load_data(fname)
 
-#         print "truncate"
-#         l = 10000
+        x -= x[0]
+
+        # cut off at 10 days
+        l = x < 10.
+        x, y, yerr = x[l], y[l], yerr[l]
+#         l = 1000
 #         x, y, yerr = x[:l], y[:l], yerr[:l]
 
-        print "convert to rv"
+        # convert to rv
         rv, rv_err = convert_to_rv(y, yerr, teff[i], t_err[i])
 
-        # convert ys to float64
-        rv64 = np.empty((len(rv)))
-        for i in range(len(rv)):
-            rv64[i] = rv[i].astype('float64')
+        # load frequencies
+        freqs = load_freqs(fname)  # Hz
 
-#         # compute periodogram
-#         f = np.logspace(.01, 2, num=1000)
-#         pgram = lombscargle(x, rv64, f*2*np.pi)
-#         # load freqs
-#         freqs = load_freqs(fname)
-#         plt.clf()
-#         plt.subplot(2, 1, 1)
-#         for freq in freqs:
-#             plt.axvline(freq*24*3600, color="r")
-#         plt.plot(f, pgram)
-#         plt.xlabel("freq")
+        # high pass filter min(freqs) = 0.0005, max(freqs) = 0.001
+        # f_crit = 0.0005
+        # filtered_rv = highpass_filter(rv, f_crit)
 
-        # high pass filter
-        freqs = load_freqs(fname)
-        # min(freqs) = 0.0005, max(freqs) = 0.001
-        f_crit = 0.0005
-        filtered_rv = highpass_filter(rv64, f_crit)
-
-#         pgram = lombscargle(x, filtered_rv, f*2*np.pi)
-#         plt.subplot(2, 1, 2)
-#         for freq in freqs:
-#             plt.axvline(freq*24*3600, color="r")
-#         plt.plot(f, pgram)
-#         plt.savefig('filter_test')
+        # fit regularised sine waves. for now fit all of them?
+        reg = 10
+        ys, A = fit_sine_reg(x*24*3600, rv, rv_err, freqs[:1]*2*np.pi, reg)
 
         plt.clf()
-        plt.subplot(2, 1, 1)
-        plt.plot(x, rv, "k.")
-        plt.subplot(2, 1, 2)
-        plt.plot(x, filtered_rv, "k.")
-        plt.savefig("test")
+        plt.plot(x, ys)
+        plt.savefig("%s" % str(int(kid[i])))
 
-        print "fit regularised sine waves"
-        reg = 1000
-        ts, ys, A = fit_freqs(x, filtered_rv, rv_err, freqs[:1]*2*np.pi, reg)
-#         ts, ys, A = fit_freqs(x, rv, rv_err, freqs*2*np.pi, reg)
+        # next, check the hd185 synthetic light curve x values
+        # save synthetic light curve in the same way maybe it doesn't
+        # matter because I'm interpolating anyway?
+        # for now just do 10 days worth again
+        np.savetxt("%s/synthesise/%s_rvs.txt" % (DIR, kid[i]),
+                   np.transpose((x, ys)))
 
 #         plt.clf()
-#         plt.plot(x, filtered_rv, "k.")
-#         plt.plot(x, ys)
-# #         plt.xlim(x[0], x[500])
-#         plt.savefig("test")
-
-        fs = np.linspace(0.00000001, max(freqs), 1000)
-        pgram = lombscargle(ts, ys, fs*2*np.pi)
-
-        plt.clf()
-        for i in freqs:
-            plt.axvline(i, color="r")
-        plt.plot(fs, pgram)
-#         plt.ylim(0, 10000)
-        plt.ylim(0, 1)
-        plt.savefig("frequency_detection")
-        assert 0
+#         plt.plot(x*24*3600, ys, "b")
+#         plt.plot(x*24*3600, rv, "k.")
+#         plt.savefig("%srv" % kid[i])
+#         f = np.linspace(.00001, 0.005, 1000)
+#         pgram = lombscargle(x*24*3600, ys, f*2*np.pi)
+#         plt.clf()
+#         plt.plot(f, pgram)
+#         plt.show()
+#         raw_input('enter')
