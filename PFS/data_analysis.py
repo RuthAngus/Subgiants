15497fe9470f9c7d32bd0a2d0d1c079cca4cdac1
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-04-13 13:59:52 -0400

    initial

--- PFS/data_analysis.py
+++ PFS/data_analysis.py
@@ -2,15 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from params import plot_params
 reb = plot_params()
+import itertools
+from gatspy.periodic import LombScargle
 
 def mean(x, y, yerr):
-    return np.mean(x), np.mean(y), np.sqrt(sum(yerr**2))
+    x, y, yerr = np.array(x), np.array(y), np.array(yerr)
+    return np.mean(x), np.mean(y), np.sqrt(sum(yerr**2))/len(x)
 
 def rms(y):
+    y = np.array(y)
     return np.sqrt(np.mean(y**2))
 
+# given a list of lists, compare each item to each other item
+def comb(x, y, yerr):
+    arrays = y
+    return list(itertools.product(*arrays))
+
 # given a time series return a list of lists
-def nights(t, rv, rverr):
+def nights(t, rv, rverr, name):
     ts, rvs, rverrs = [], [], []
     diff = np.diff(t)
     x = np.where(diff > .5)[0] + 1
@@ -22,97 +31,63 @@ def nights(t, rv, rverr):
         ts.append(t[x[i]:x[i+1]])
         rvs.append(rv[x[i]:x[i+1]])
         rverrs.append(rverr[x[i]:x[i+1]])
+    if name == "HD98219":
+        ts.append(t[:3])
+        rvs.append(rv[:3])
+        rverrs.append(rverr[:3])
+    ts.append(t[-3:])
+    rvs.append(rv[-3:])
+    rverrs.append(rverr[-3:])
     return ts, rvs, rverrs
 
-def cm(x):
-    s = 3
-    n = 2
-    ncombinations = s**n
-    if len(x) > 6:
-        n = 3
-        c = range(1, s+1) + range(1, s+1) + range(1, s+1)
-        comb = np.zeros((ncombinations, n))
-        comb[:, 1] = np.sort(np.array(c))
-        c = range(s+1, 2*s+1) + range(s+1, 2*s+1) + range(s+1, 2*s+1)
-        comb[:, 2] = np.array(c)
-    if len(x) < 7:
-        c = range(s) + range(s) + range(s)
-        comb = np.zeros((ncombinations, n))
-        comb[:, 0] = np.sort(np.array(c))
-        c = range(s, 2*s) + range(s, 2*s) + range(s, 2*s)
-        comb[:, 1] = np.array(c)
-    return comb
+def make_plot(t, rv, rverr, D, fname):
+
+    t0 = 2457061
+    t -= t0
+
+    # split into list of nights
+    ts, rvs, rverrs = nights(t, rv, rverr, fname)
+
+    c = comb(ts, rvs, rverrs)
+
+    # bin nightly observations
+    bt = [mean(ts[i], rvs[i], rverrs[i])[0] for i in range(len(ts))]
+    brv = [mean(ts[i], rvs[i], rverrs[i])[1] for i in range(len(ts))]
+    brverr = [mean(ts[i], rvs[i], rverrs[i])[2] for i in range(len(ts))]
+
+    # make plot
+    plt.clf()
+    plt.errorbar(t, rv, yerr=rverr, **reb)
+    plt.errorbar(bt, brv, yerr=brverr, fmt="r.", capsize=0, ecolor=".7")
+    for i in c:
+        plt.axhline(rms(i), color="k", alpha=.01)
+        plt.axhline(rms(i)/np.sqrt(3), color="b", alpha=.01)
+    plt.axhline(rms(brv), color="r")
+
+    plt.xlabel("$\mathrm{JD}-%s$" % t0)
+    plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
+    print "%s/%s" % (D, fname)
+    plt.savefig("%s/%s" % (D, fname))
+
+def pgram(t, y, dy, fname):
+#     t *= 24*3600  # convert to seconds
+#     fs = np.arange(50, 1000, .1)*1e-6  # Hz
+#     period = 1. / fs
+#     plt.plot(fs, power)
+    model = LombScargle().fit(t, y, dy)
+    period = np.linspace(.25/24, 5./24, 1000)
+    fs = 1./period
+    power = model.periodogram(period)
+    plt.clf()
+    plt.plot(period*24., power)
+    plt.savefig("%s_pgram" % fname)
+    print period[power==max(power)]*24
 
 if __name__ == "__main__":
 
     D = "/Users/angusr/Python/Subgiants/paper"
-
-    t1, rv1, rverr1 = np.genfromtxt("HD95089_PFS2.vels").T
-    t2, rv2, rverr2 = HD98219 = np.genfromtxt("HD98219_PFS2.vels").T
-
-    t0 = 2457061
-    t1 -= t0
-    t2 -= t0
-
-    # split into list of nights
-    ts1, rvs1, rverrs1 = nights(t1, rv1, rverr1)
-    ts2, rvs2, rverrs2 = nights(t2, rv2, rverr2)
-
-    bt1, brv1, brverr1 = [t1[0]], [rv1[0]], [rverr1[0]]
-    b = mean(t1[1:4], rv1[1:4], rverr1[1:4])
-    bt1.append(b[0])
-    brv1.append(b[1])
-    brverr1.append(b[2])
-    b = mean(t1[5:], rv1[5:], rverr1[5:])
-    bt1.append(b[0])
-    brv1.append(b[1])
-    brverr1.append(b[2])
-
-    bt1, brv1, brverr1 = np.array(bt1), np.array(brv1), np.array(brverr1)
-
-    c1 = cm(t1)
-    rmss1 = []
-    for c in c1:
-        l = np.array([int(i) for i in c])
-        rmss1.append(rms(rv1[l]))
-
-    plt.clf()
-    plt.errorbar(t1, rv1, yerr=rverr1, **reb)
-    plt.errorbar(bt1, brv1, yerr=brverr1, fmt="r.", capsize=0, ecolor=".7")
-    plt.axhline(rms(brv1), color="r")
-    for r in rmss1:
-        plt.axhline(r, color=".7")
-    plt.xlabel("$\mathrm{JD}-%s$" % t0)
-    plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
-    plt.savefig("%s/HD95089.pdf" % D)
-
-    bt2, brv2, brverr2 = [], [], []
-    b = mean(t2[:3], rv2[:3], rverr2[:3])
-    bt2.append(b[0])
-    brv2.append(b[1])
-    brverr2.append(b[2])
-    b = mean(t2[4:], rv2[4:], rverr2[4:])
-    bt2.append(b[0])
-    brv2.append(b[1])
-    brverr2.append(b[2])
-
-    bt2, brv2, brverr2 = np.array(bt2), np.array(brv2), np.array(brverr2)
-
-    c2 = cm(t2)
-    rmss2 = []
-    for c in c2:
-        l = np.array([int(i) for i in c])
-        rmss2.append(rms(rv2[l]))
-
-    plt.clf()
-    plt.errorbar(t2, rv2, yerr=rverr2, **reb)
-    plt.errorbar(bt2, brv2, yerr=brverr2, fmt="r.", capsize=0, ecolor=".7")
-    plt.axhline(rms(brv2), color="r")
-    for r in rmss2:
-        plt.axhline(r, color=".7")
-    plt.xlabel("$\mathrm{JD}-%s$" % t0)
-    plt.ylabel("$\mathrm{RV~(ms}^{-1}\mathrm{)}$")
-    plt.savefig("%s/HD98219.pdf" % D)
-
-    print rv1
-    print rv2
+    fnames = ["HD95089", "HD98219"]
+    for fname in fnames:
+        t, rv, rverr = np.genfromtxt("%s_PFS2.vels" % fname).T
+        pgram(t, rv, rverr, fname)
+#         make_plot(t, rv, rverr, D, fname)
