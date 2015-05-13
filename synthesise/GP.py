3e3468d1cde8b69c66ad011ad3403b116b901526
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-05-13 12:09:54 -0400

    using physical mode lifetimes

--- synthesise/GP.py
+++ synthesise/GP.py
@@ -6,7 +6,6 @@ import scaling_relations as sc
 import scipy.signal as sps
 
 D = "/Users/angusr/Python"
-
 ndays = 10
 xs = np.linspace(0, ndays, 2000)
 yerr = np.ones_like(xs)*1.
@@ -21,35 +20,15 @@ T, T_err = data[1], data[2]
 M, M_err = data[9], data[10]
 R, R_err = data[13], data[14]
 
+# mode lifetime scaling relation
+tau = 3.2 * (T/5780.)**-4  # in days
 nm = 24*3600*sc.nu_max(M, R, T)*1e-3  # in days-1
 
 for i in range(len(kids)):
     print kids[i], i, "of", len(kids)
-
-    theta = [5, 1, 1, 1./nm[i]]
-    k = theta[0]*ExpSquaredKernel(theta[1])*ExpSine2Kernel(theta[2], theta[3])
+    k = 5.*ExpSquaredKernel(tau[i])*CosineKernel(1./nm[i])
     gp = george.GP(k)
     gp.compute(xs, yerr)
     ys = gp.sample()
-    plt.clf()
-    plt.subplot(2, 1, 1)
-    plt.plot(xs, ys)
-
-#     k = theta[0]*ExpSquaredKernel(theta[1])*CosineKernel(2*np.pi*nm[i])
-    k = theta[0]*ExpSquaredKernel(theta[1])*CosineKernel(theta[3])
-    gp = george.GP(k)
-    gp.compute(xs, yerr)
-    ys2 = gp.sample()
-    plt.plot(xs, ys2)
-
-    print nm[i]
-    plt.subplot(2, 1, 2)
-    fs = np.linspace(.1, 2*nm[i], 1000)
-    pgram1 = sps.lombscargle(xs, ys, fs*2*np.pi)
-    pgram2 = sps.lombscargle(xs, ys2, fs*2*np.pi)
-    plt.plot(fs, pgram1)
-    plt.plot(fs, pgram2)
-    plt.show()
-    raw_input('neter')
-#     np.savetxt("%s/Subgiants/injections/%s_%s_GP.txt" % (D, kids[i], ndays),
-#                np.transpose((xs, ys)))
+    np.savetxt("%s/Subgiants/injections/%s_%s_GP.txt" % (D, kids[i], ndays),
+               np.transpose((xs, ys)))
