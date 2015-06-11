8f4fc16967666a5c37c870062d7b538dce58e64f
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-06-11 15:53:20 -0400

    selecting for August PFS run

--- PFS/PFS_selection.py
+++ PFS/PFS_selection.py
@@ -39,6 +39,11 @@ def select():
     l = (dec<30) * (6.5<v) * (v<8.5) * (1<m) * (m<1.8) \
             * (ramin<ra) * (ra<ramax)
 
+    # August run
+    ramin, ramax = 300, 340
+    l = (dec<30) * (6.5<v) * (v<8.5) * (1<m) * (m<1.8) \
+            * (ramin<ra) * (ra<ramax)
+
     print len(name), "targets"
     return name[l], ra[l], dec[l], m[l], v[l], R[l], T[l]
 
@@ -58,31 +63,12 @@ if __name__ == "__main__":
     for i in range(len(name)):
         print name[i], ra[i], dec[i], v[i]
         print "M = ", m[i], "R = ", R[i], "T = ", T[i]
-        print "nm = ", sr.nu_max(m[i], R[i], T[i])*1e3
+        nm = sr.nu_max(m[i], R[i], T[i])*1e3
+        print "nm = ", nm
         print "dn = ", sr.delta_nu(m[i], R[i])
+        print "1/2 period = ", 1./(nm * 1e-6) / 60. / 2., "mins"
         print "exptime = ", exptime[i], "\n"
         etimes.append(exptime[i])
 
     print len(name), "targets"
     np.savetxt("exptimes.txt", np.array(etimes))
-
-# JAN 27, 2014       20:40:31       -18:20: 4
-# JAN 28, 2014       20:44:38       -18: 4:19
-# JAN 29, 2014       20:48:46       -17:48:10
-# JAN 30, 2014       20:52:53       -17:31:43
-# JAN 31, 2014       20:56:58       -17:15: 1
-# FEB  1, 2014       21: 1: 3       -16:57:56
-# FEB  2, 2014       21: 5: 7       -16:40:33
-# FEB  3, 2014       21: 9:10       -16:22:58
-# FEB  4, 2014       21:13:12       -16: 5: 0
-# FEB  5, 2014       21:17:14       -15:46:47
-# FEB  6, 2014       21:21:14       -15:28:21
-# FEB  7, 2014       21:25:14       -15: 9:35
-# FEB  8, 2014       21:29:14       -14:50:34
-# FEB  9, 2014       21:33:11       -14:31:23
-# FEB 10, 2014       21:37: 9       -14:11:52
-# FEB 11, 2014       21:41: 6       -13:52: 7
-# FEB 12, 2014       21:45: 1       -13:32:13
-# FEB 13, 2014       21:48:56       -13:12: 1
-# FEB 14, 2014       21:52:51       -12:51:35
-# FEB 15, 2014       21:56:44       -12:31: 3
